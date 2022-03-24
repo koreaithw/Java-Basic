e3886115b988e901d3de1f969a7aad6a6287be2a
kimhy <aangie4@naver.com>
2022-03-24 21:54:53 +0900

    java

---
+++ javahw/src/javahw/test1.java
@@ -0,0 +1,46 @@
+package javahw;
+
+import java.util.HashSet;
+
+class set{
+	HashSet<Integer> hs = new HashSet<Integer>();
+	HashSet<Integer> hs1 = new HashSet<Integer>();
+	
+	
+	public HashSet<Integer> hash() {
+		for(int i=0;i<5;i++) {
+			hs.add(i+1);
+		}
+		for(int i=0;i<5;i++) {
+			hs1.add(i+3);
+		}
+		hs.removeAll(hs1);
+		return hs;
+	}
+}
+class revers{
+	public int rev(int a) {
+		int index=0;
+		if(a<0) index=1;
+		String str = String.valueOf(a);
+		String str1="";
+		int b = str.length();
+		for (int i = b-1; i >=index ; i--) {
+			str1+=str.charAt(i);
+		}
+		int res = Integer.valueOf(str1);
+		return res;
+	}
+	
+}
+public class test1 {
+	
+	public static void main(String[] args) {
+		set s = new set();
+		System.out.println(s.hash().toString());
+		
+		revers r = new revers();
+		System.out.println(r.rev(-123));
+		System.out.println(r.rev(123));
+	}
+}
